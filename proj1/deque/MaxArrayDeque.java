c6f96035a09d5f06f8cf5103b29ac1bfc49b651a
Shansheng <2422327062@qq.com>
2025-03-31 20:15:07 +0800

    update the comment

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -2,33 +2,38 @@ package deque;
 
 import java.util.Comparator;
 
-public class MaxArrayDeque<T> extends ArrayDeque<T> {
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
     private final Comparator<T> comparator;
 
-    public MaxArrayDeque(Comparator<T> c){
+    public MaxArrayDeque(Comparator<T> c){ // Constructor
         super();
         this.comparator = c;
     }
 
+    /* if we want to compare the element in the deque,we need a comparator
+       below are two different ways to call the comparator
+     */
+    public T max(){ // in this method we need to figure out how to pass in a comparator
+        // just say return maxHelper(); ? nope we need a comparator ,so make a comparator above!
+        return maxHelper(comparator);// YES! that is!
+    }
+
+    public T max(Comparator<T> c){ // in this method we have had a comparator
+        return maxHelper(c);
+    }
+
+    // we need a helper
     private T maxHelper(Comparator<T> c){
-        if (isEmpty()){
+        if (isEmpty()){// when the deque is empty
             return null;
         }
         T ans = get(0);
         for (int i = 1; i < size(); i ++){
             T tmp = get(i);
-            if (c.compare(ans, tmp) > 0){
+            if (c.compare(tmp , ans) > 0){
                 ans = tmp;
             }
         }
         return ans;
-    }
-
-    public T max(){
-        return maxHelper(this.comparator);
-    }
-
-    public T max(Comparator<T> c){
-        return maxHelper(c);
     }
 }
